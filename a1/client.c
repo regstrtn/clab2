f4e29499bd063e5f4c075b8103193b2fa01e7724
regstrtn <regstrtn@gmail.com>
2017-01-11 20:28:33 +0530

    Add files via upload

--- a1/client.c
+++ a1/client.c
@@ -13,17 +13,17 @@ int wellformatted(char *buffer) {
 	for(i=0;i<strlen(buffer); i++) {
 		if(buffer[i] == ';') count++;
 	}
-	if(count == 1 && buffer[strlen(buffer)-2] == ';') return 0;
-	if(count == 1 && buffer[0] == ';') return 0;
-	if(count>1) return 0;
-	if(count<1) return 0;
+	if(count == 1 && buffer[strlen(buffer)-2] == ';') return 0;			//if no pattern
+	if(count == 1 && buffer[0] == ';') return 0;						//if no text
+	if(count>1) return 0;												//if multiple delimiters
+	if(count<1) return 0;												//if no delimiter
 	return 1;
 }
 
 void readinput(char *buffer) {
 	int len;
 	getline(&buffer, &len, stdin);
-	while(!wellformatted(buffer)) {
+	while(!wellformatted(buffer)) {												//Validate user input
 		printf("Enter input in the following format - 'haystack;needle': ");
 		getline(&buffer, &len, stdin);
 	}
@@ -31,31 +31,32 @@ void readinput(char *buffer) {
 	strcpy(op, buffer);
 	char *haystack = strtok(op, ";");
 	char *needle = strtok(NULL, ";");
-	printf("Haystack: %s Needle: %s ", haystack, needle);
+	printf("Haystack: %s Needle: %s", haystack, needle);
 }
 
 int main() {
 	int sockfd, newsockfd, n, cli_len, port_no;
 	struct sockaddr_in serv;
+	
+	//Server Information. Port 5000;
 	port_no = 5000;
 	char buffer[256], haystack[256], needle[256];
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	struct hostent *server;		//<netdb.h>
-	server = gethostbyname("127.0.0.1");
+	struct hostent *server;							//<netdb.h>
+	server = gethostbyname("127.0.0.1"); 			//localhost
 	bzero((char*)&serv, sizeof(serv));
 	//AF_INET - IPv4. SOCK_STREAM - TCP Protocol. 0 - Assign a protocol for the address type, family combination. Here TCP will be assigned.  
+
 	serv.sin_family = AF_INET;
-	
 	bcopy((char *)server->h_addr, (char *) &serv.sin_addr.s_addr, server->h_length);
 	serv.sin_port = htons(port_no);
+	
+
 	bzero(buffer, 256);
-
-	readinput(buffer); //Process user input
-
+	readinput(buffer); 								//Process user input and store in buffer
 	connect(sockfd, (struct sockaddr *) &serv, sizeof(serv));
 	write(sockfd, buffer, strlen(buffer));
 	bzero(buffer, 256);
 	read(sockfd, buffer, 255);
 	printf("Occurrences: %s\n", buffer);
-	//write(newsockfd, "Got your message\n", 19);
 }
--- a1/server.c
+++ a1/server.c
@@ -8,17 +8,17 @@
 
 int search(char buffer[]) {
 	int count = 0;
-	//strstr(char *haystack, char *needle)
 	char *token, *haystack, *needle;
 	char s[2] = ";";
 	printf("%s\n", buffer);
+	
+	/*Split input into haystack and needle*/
 	haystack = strtok(buffer, s);
 	needle = strtok(NULL, s);
 	needle[strlen(needle)-1] = '\0';
 	char *tmp = haystack;
 	while((tmp = strstr(tmp, needle))!=NULL) {
 		tmp++;
-		//printf("Stillcounting\n");
 		count++;
 	}
 	return count;
@@ -41,21 +41,28 @@ int main() {
 	bind(sockfd, (struct sockaddr*)&serv, sizeof(serv));
 	int pid;
 	listen(sockfd, 5);
-	while(1) { //continue to loop forever
+
+	while(1) { 											//continue to loop forever
 		newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, &cli_len);
-		if((pid = fork()) > 0) {
+		
+		if((pid = fork()) > 0) { 						//Parent
 			close(newsockfd); 
 			continue;
-		} 
-		else if(pid == 0) {
+		}
+		else if(pid == 0) {								//Child
 			bzero(buffer, 256);
 			read(newsockfd, buffer, 255);
 			printf("Client message: %s\n", buffer);
-			int n = search(buffer);
+			int n = search(buffer);						//Count occurrences of pattern in text
 			bzero(buffer, 256);
 			sprintf(buffer, "%d\0", n);
 			printf("My response: %s\n", buffer);
 			write(newsockfd, buffer, sizeof(buffer));
+			break;
+		}
+		else if(pid<0) {								//Fork fail
+			close(newsockfd);
+			write(newsockfd, "Fork call failed\n", sizeof(buffer));
 			break;
 		}
 	}
