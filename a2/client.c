33a34646d7bd6188dcf5503adea9666e01d4b679
regstrtn <regstrtn@gmail.com>
2017-01-20 12:09:32 +0530

    Add files via upload

--- a2/client.c
+++ a2/client.c
@@ -47,8 +47,13 @@ int main() {
 		int i;
 		for(i = 0;i<2;i++) {
 			if(fds[i].revents & POLLIN) {
+				bzero((char*)incoming, 256);
 				read(fds[i].fd, incoming, 255);
-				if(i==0) {							//Read message from server
+				if(i==0) {	
+					if(incoming[0] == 0) {
+						printf("Server disconnected\n");
+						exit(0);
+					}	
 					printf("Incoming message from %d: %s\n", fds[i].fd, incoming);
 					bzero((char*)incoming, 256);
 				}
--- a2/smain.c
+++ a2/smain.c
@@ -14,6 +14,7 @@
 #include <netinet/in.h>
 #include <fcntl.h>
 #include "sighandler.c"
+#define MAXCLIENTS 2
 
 typedef struct {
 	int id;
@@ -32,29 +33,48 @@ typedef struct {
 } msg;
 
 void printclientdetails(cli *a) {
-	printf("Printing details of client %s: ", a->name);
+	printf("Printing details of %s: ", a->name);
 	printf("%d %d %ld\n", a->id, a->fd, a->conntime);
+}
+
+char* showonline(cli* clilist) {
+	cli *curr = clilist;
+	int i;
+	char *b = (char*)calloc(256,sizeof(char));
+	strcat(b, "Online users: \n");
+	for(i=0;i<MAXCLIENTS;i++) {
+		strcat(b, (curr+i)->name); 
+		strcat(b, "\n");
+	}
+	printf("%s", b);
+	return b;
 }
 
 void retrievemessages(msg *mbuffer, int *q, cli* clilist) {
 	//Send message to correct client
 	msg *curr = mbuffer + q[1];
 	int i;
-	//while(q[0]<100) {
-	/*	if(q[1]>=q[0]) {
-			break;
+	while(1) {
+		if(q[1]==q[0]) {
 			usleep(100*1000); 
 			continue;
-		}*/
-		q[1]++;
+		}
 		char *r = curr->recname;
 		for(i=0;i<5;i++) {
-			if(strcmp(clilist[i].name, r)==0) break;
+			if(strcmp(clilist[i].name, r)==0) {
+				break;
+			}
 		}
-		if(i==5) printf("Receiver does not exist\n");
-		write(clilist[i].fd, curr->message, 255);
+		if(i==5) printf("Receiver %s does not exist\n", r);
+		else {
+			printf("Value of i: %d\n", i);
+			printf("Intended receiver: %s id: %d fd: %d\n", clilist[i].name, clilist[i].id, clilist[i].fd);
+			printf("Message: %s\n", curr->message);
+		}
+		//write(clilist[i].fd, curr->message, 255);
 		curr = curr+1;
-	//}
+		q[1]++;
+	}
 }
 
 void enqueue(msg newmsg, msg* mbuffer, int *q) {
@@ -62,6 +82,7 @@ void enqueue(msg newmsg, msg* mbuffer, int *q) {
 	msg* curr = mbuffer + q[0];
 	q[0]++;
 	strcpy(curr->message, newmsg.message);
+	strcpy(curr->recname, newmsg.recname);
 }
 
 char* getrecname(char *rawmsg) {
@@ -73,28 +94,50 @@ char* getrecname(char *rawmsg) {
 	return cliname;
 }
 
+char *getmsg(char *rawmsg) {
+	int colonpos = (int)(strchr(rawmsg, ':')-rawmsg);
+	char *msg = (char*)malloc(256*sizeof(char));
+	strncpy(msg, rawmsg+colonpos+1, strlen(rawmsg)-colonpos);
+	msg[100] = '\0';
+	return  msg;
+}
+
 void fillclientdetails(cli *a, int *ctr, int newsockfd) {
 	a->id = *ctr;
-	sprintf(a->name, "client%d ", *ctr);
+	sprintf(a->name, "client%d", *ctr);
 	a->fd = newsockfd;
 	a->conntime = time(0)%10000;
+	printclientdetails(a);
+	showonline(a-(*ctr));
 }
 
 void handleclients(cli* clilist, int* ctr, msg* mbuffer, int *q) {
 	cli* me = clilist+((*ctr)-1);
-	printf("Printing my information: %s\n", me->name);
-	printf("Receiver name: %s\n", getrecname("Hello:World"));
-	write(me->fd, me->name, 30);
+	char b[256] = {0}; 
+	/*
+	while(1) {
+		bzero((char*)b, 256);
+		read(me->fd, b, 255);
+		write(me->fd, b, 255);
+	} */
+	sprintf(b, "ID: %d Name: %d fd: %d\n", me->id, me->name, me->fd);
+	write(me->fd, b, 256);
+	write(me->fd, showonline(clilist), 256);
 	int counter = 0;
 	while(1) {
 		msg newmsg;
 		read(me->fd, newmsg.message, 255);
+		if(strcmp(newmsg.message, "+online")==0) {
+			showonline(clilist-(*ctr));
+		}
 		char* rec = getrecname(newmsg.message);
+		char *msgtxt = getmsg(newmsg.message);
 		strcpy(newmsg.recname, rec);
+		strcpy(newmsg.message, msgtxt);
 		newmsg.mtime = time(0);
 		enqueue(newmsg, mbuffer, q);
 		counter++;
-		if(counter%3==0) retrievemessages(mbuffer, q, clilist);
+		//if(counter%3==0) retrievemessages(mbuffer, q, clilist);
 	}
 
 }
@@ -140,13 +183,16 @@ int main() {
 	int *q = (int*)shmat(qid, 0, 0);
 	*ctr = 0;
 	q[0] = 0; q[1] = 0;
+	
+	int pid1 = fork(); 							//Create a new process P for delivering messages
+	if(pid1==0) {
+		retrievemessages(mbuffer, q, clilist);
+	}
 	while(1) {
 		sleep(1);
 		newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
 		if(allowconnection(ctr)) {
-			//write(newsockfd, "3 clients connected\n", 40); 
 			fillclientdetails(clilist+(*ctr), ctr, newsockfd);
-			printclientdetails(clilist+(*ctr));
 			(*ctr)++;
 			pid = fork();
 			if(pid == 0) {	//Child process
